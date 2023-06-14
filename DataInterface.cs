0908e1c5d94c01f22b8051ad4d5e64040e12fd53
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-06-14 15:21:44 +0300

    Add files via upload

---
+++ DataInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDesktop.interfaces
+{
+    static class DataInterface
+    {
+        public static string tkn = "";
+        public static string role = "";
+    }
+}
